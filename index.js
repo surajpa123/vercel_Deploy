69ce1ac50cd267f891be06738be61e00c55d7e2c
surajpa123 <pathaksuraj136@gmail.com>
2024-06-14 15:18:54 +0530

    added

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const PORT = 4000
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Backend');
+
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app
